44d9b75bddd49b82bcabf8c268a4710e22c83f0d
donjoo <donjorois@gmail.com>
2025-08-06 04:19:41 +0530

    done ticket views

--- ticket_system/tickets/views.py
+++ ticket_system/tickets/views.py
@@ -1,6 +1,14 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions , viewsets , filters , status
+from rest_framework.response import Response
 from django.contrib.auth.models import User
 from rest_framework.serializers import ModelSerializer
+from django_filter.rest_framework import DjangoFilterBackend
+from .models import Ticket
+from .serializers import TicketSerializer
+from .permissions import IsOwnerOrAdmin
+
+from asgiref.sync import async_to_sync
+from  channels.layers import get_channel_layer
 
 
 
@@ -39,3 +47,57 @@ class UserProfileView(generics.RetrieveAPIView):
 
     def get_object(self):
         return self.request.user
+
+
+
+# Ticket Views
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.select_related('user', 'assigned_to').all()
+    serializer_class = TicketSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrAdmin]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['status', 'priority','user', 'assigned_to']
+    search_fields = ['title', 'description']
+    ordering_fields = ['created_at', 'priority', 'status']
+
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return self.queryset
+        return self.queryset.filter(user=user)
+
+    def perform_create(self, serializer):
+        ticket = serializer.save(user=self.request.user)
+        self._broadcast('created',ticket)
+
+
+    def perform_update(self, serializer):
+        ticket = serializer.save()
+        self._broadcast('updated', ticket)  
+
+
+    def perform_destroy(self, instance):
+        ticket_data = TicketSerializer(instance).data
+        instance.delete()   
+        self._broadcast('deleted', ticket_data,serialized=True)
+
+    def _broadcast(self, action , ticket_obj,serialized=False):
+        layer = get_channel_layer()
+        if serialized:
+            payload = ticket
+        else:
+            password = TicketSerializer(ticket_obj).data
+        message = {
+            'action': action,
+            'ticket': payload
+        }
+        async_to_sync(layer.group_send)(
+            'ticket_updates', 
+            {
+                'type': 'ticket_update',
+                'message': message
+            }  
+        )
+        
