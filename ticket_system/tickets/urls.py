a7bb5ce36635129094a6269cf3e075175c79ca9b
donjoo <donjorois@gmail.com>
2025-08-06 16:58:24 +0530

    done routing-urls-asgi

--- ticket_system/tickets/urls.py
+++ ticket_system/tickets/urls.py
@@ -3,6 +3,7 @@ from .views import RegisterUserView, UserProfileView
 
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('register/', RegisterUserView.as_view(), name='register_user'),
     path('me/', UserProfileView.as_view(), name='user_profile'),
 
