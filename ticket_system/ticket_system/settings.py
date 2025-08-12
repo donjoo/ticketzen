6825ec64f7609ab38fb05e8d9f677d5f52643ae3
donjoo <donjorois@gmail.com>
2025-08-12 10:55:31 +0530

    updated allowed_hosts

--- ticket_system/ticket_system/settings.py
+++ ticket_system/ticket_system/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [    "ticket_system.onrender.com","localhost", "127.0.0.1",    "localhost:5173"]
+ALLOWED_HOSTS = [  "ticketzen.onrender.com" ,"localhost", "127.0.0.1",    "localhost:5173"]
 
 
 # Application definition
