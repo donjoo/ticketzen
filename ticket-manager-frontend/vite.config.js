45ab5e712a8db672bb0f7709c7ff70665a6e0dc3
donjoo <donjorois@gmail.com>
2025-10-11 18:59:53 +0530

    Update vite.config.js,added vercel.json

--- ticket-manager-frontend/vite.config.js
+++ ticket-manager-frontend/vite.config.js
@@ -2,12 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 5173,
+    open: true,
+    // 👇 this ensures fallback to index.html for SPA routing in dev mode
+    historyApiFallback: true,
+  },
+  preview: {
+    port: 4173,
+    // 👇 this ensures fallback to index.html for SPA routing in preview mode
+    historyApiFallback: true,
+  }
 })
